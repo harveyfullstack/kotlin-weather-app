1ac0604099c63a096a780bcbb73d7adf1cb39d2a
Jeremiah Harvey <harveyfullstack@gmail.com>
2025-03-31 17:21:17 -0400

    Fix formatting

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-
+    
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
